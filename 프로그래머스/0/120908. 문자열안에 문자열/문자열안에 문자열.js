b485f401823ec2f1f286d204d6271eb613e209e6
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-24 13:38:31 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -0,0 +1,29 @@
+function solution(str1,str2){
+    return str1.includes(str2)? 1 : 2;
+}
+
+
+
+
+
+
+
+
+
+
+/*function solution(str1,str2){
+    return str1.includes(str2)?1:2;
+}*/
+
+
+
+
+
+
+
+
+
+
+/*function solution(str1, str2) {
+    return str1.indexOf(str2) >= 0 ? 1 : 2;
+} */
