d33bf93ad708521ce8f41404865ad77ccbaaccd9
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-25 20:30:42 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,3 +1,12 @@
-function solution(s1, s2) {
-    return s1.filter(el => s2.includes(el)).length;
+function solution(s1,s2){
+    return s1.filter(v => s2.includes(v)).length;
 }
+
+
+
+
+
+
+/*function solution(s1, s2) {
+    return s1.filter(el => s2.includes(el)).length;
+}*/
