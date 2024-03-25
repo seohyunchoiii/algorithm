6f8f8f864fb4fe0f5fd692f26763ffc847410d38
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-25 20:37:21 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,3 +1,15 @@
-function solution(n) {
-    return String(n).split('').reduce((acc, cur) => (+acc) + (+cur),0);
+function solution(n){
+    return String(n).split('').reduce((acc,cur) => (+acc) + (+cur),0);
+    
+    
+    
+    
 }
+
+
+
+
+
+/*function solution(n) {
+    return String(n).split('').reduce((acc, cur) => (+acc) + (+cur),0);
+}*/
