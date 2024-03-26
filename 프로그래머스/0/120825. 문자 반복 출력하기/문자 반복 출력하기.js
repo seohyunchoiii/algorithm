cdb792dea4e292bd29631ce78fd551996f9f6612
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-26 20:26:08 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,10 +1,27 @@
 function solution(my_string,n){
     let answer = "";
+    for(var i=0; i<my_string.length;i++){
+         answer += my_string[i].repeat(n);
+    }
+    return answer;
+}
+
+
+
+
+
+
+
+
+
+
+/*function solution(my_string,n){
+    let answer = "";
     for(let a of my_string){
         answer += a.repeat(n);
     }
 return answer;
-}
+}*/
 
 
 
