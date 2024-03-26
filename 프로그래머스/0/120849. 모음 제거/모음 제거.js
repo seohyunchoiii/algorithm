7780e8012cdbe699170edcb4d8dfbe6d31183d00
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-26 20:36:33 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,4 +1,18 @@
-function solution(my_string) {
+function solution(my_string){
+    return my_string.replace(/[a,e,i,o,u]/g, "");
+}
+
+
+
+
+
+
+
+
+
+
+
+/*function solution(my_string) {
     let vowel =["a", "e", "i", "o", "u"];
     return my_string.split("").filter(v => !(vowel.includes(v))).join("");
-}
+}*/
