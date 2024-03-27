2fd96fba497967909954930eda3617bbc6825f6d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-27 21:02:47 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,15 @@
-function solution(my_string) {
-    return my_string.split('').filter(v => v*1>=1).reduce((acc,cur) => (+acc)+(+cur));
+function solution(my_string){
+    return my_string.split("").filter(v => v*1 > 0).reduce((acc, cur) => (+acc) +(+cur));
 }
+
+
+
+
+
+
+
+
+
+/*function solution(my_string) {
+    return my_string.split('').filter(v => v*1>=1).reduce((acc,cur) => (+acc)+(+cur));
+}*/
