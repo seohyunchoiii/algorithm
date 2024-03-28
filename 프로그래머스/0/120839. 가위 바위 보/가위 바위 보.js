8d80c459f50ccdbff5c6a4781c70692efbae23d1
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-28 20:48:05 +0900

    [level 0] Title: 가위 바위 보, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,3 +1,16 @@
-function solution(rsp) {
-    return [...rsp].map( (v) => v==5 ? 2 : (v==2 ? 0 : 5)).join('');
+function solution(rsp){
+    var arr = {
+        2:0,
+        0:5,
+        5:2};
+    var answer = [...rsp].map( v => arr[v]).join("");
+    
+    return answer;
 }
+
+
+
+
+/*function solution(rsp) {
+    return [...rsp].map( (v) => v==5 ? 2 : (v==2 ? 0 : 5)).join('');
+}*/
