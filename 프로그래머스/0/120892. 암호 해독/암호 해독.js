8c5e08ceeff145d3266bd0da9e9cce56b0cbc444
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-28 20:37:12 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,8 +1,19 @@
-function solution(cipher, code) {
+function solution(cipher,code){
+    var answer = "";
+    cipher = cipher.split("");
+    for(var i=code-1; i<cipher.length; i+=code){
+        answer += cipher[i];
+    }
+    return answer;
+}
+
+
+
+/*function solution(cipher, code) {
     var answer = "";
     cipher = cipher.split("");
     for(var i=code-1; i< cipher.length; i+=code){
         answer += cipher[i];
     }
         return answer;
-}
+}*/
