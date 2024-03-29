5aac51b4aa86f14f628ca7526885d6fd6f864c4b
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-29 21:59:36 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,12 +1,16 @@
 function solution(rsp){
-    var arr = {
+    let arr = {
         2:0,
-        0:5,
-        5:2};
-    var answer = [...rsp].map( v => arr[v]).join("");
-    
-    return answer;
+        5:2,
+        0:5
+    }
+    return rsp.split('').map(v => arr[v]).join('');
 }
+
+
+
+
+
 
 
 
