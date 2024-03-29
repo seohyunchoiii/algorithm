467292485fbe047ba5601c42f10bd9ad562ec953
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-29 22:10:01 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,19 @@
-function solution(hp) {
+function solution(hp){
+    return Math.floor(hp/5) + Math.floor(hp%5/3) + Math.floor((hp%5)%3);
+}
+
+
+
+
+
+
+
+
+
+/*function solution(hp) {
     var answer = 0;
     var i = Math.floor(hp/5);
     var j = Math.floor(hp%5/3);
     var k = Math.floor(hp%5%3/1);
     return answer = i+j+k;
-    }
+    }*/
