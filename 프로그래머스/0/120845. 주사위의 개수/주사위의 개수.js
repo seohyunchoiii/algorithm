2b21a40a356c2cc73cfb23fb8cc7060919c185d5
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-03-30 14:04:40 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,10 @@
-function solution(box, n) {
-    return Math.floor((box[0]/n)) * Math.floor((box[1]/n)) * Math.floor((box[2]/n));
+function solution(box, n){
+    return Math.floor((box[0]/n))*Math.floor((box[1]/n))*Math.floor((box[2]/n));
 }
+
+
+
+
+/*function solution(box, n) {
+    return Math.floor((box[0]/n)) * Math.floor((box[1]/n)) * Math.floor((box[2]/n));
+}*/
