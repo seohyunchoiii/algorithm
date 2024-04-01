c0760fdac1131705bb42d4fd68ae12177d64ed60
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-01 21:23:03 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,5 +1,18 @@
-function solution(my_string, num1, num2) {
-    const arr = [...my_string];
+function solution(my_string,num1,num2){
+    const arr = my_string.split('');
     [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
     return arr.join('');
 }
+
+
+
+
+
+
+
+
+/*function solution(my_string, num1, num2) {
+    const arr = [...my_string];
+    [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+    return arr.join('');
+}*/
