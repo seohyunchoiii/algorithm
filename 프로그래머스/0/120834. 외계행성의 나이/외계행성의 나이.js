d0ff56d3bf6e7ee45ca21c0f59b1e90d18abe1f9
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-03 20:37:51 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,10 +1,24 @@
-function solution(age) {
-    var answer = '';
-    var alphabet = ['a','b','c','d','e','f','g','h','i','j'];
-    var age = String(age);
-    for (let a of age) {
-        answer += alphabet[a];
-    }
+function solution(age){
+    return age.toString().split("").map(v => "abcdefghij"[v]).join("");
+ 
+
     
-    return answer;
 }
+
+
+
+
+
+
+
+
+// function solution(age) {
+//     var answer = '';
+//     var alphabet = ['a','b','c','d','e','f','g','h','i','j'];
+//     var age = String(age);
+//     for (let a of age) {
+//         answer += alphabet[a];
+//     }
+    
+//     return answer;
+// }
