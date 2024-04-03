50dba80e141bf3f2004219b846c3f9fd327bcf86
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-03 20:47:02 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,3 +1,9 @@
-function solution(num, k) {
-   return String(num).includes(k) ? String(num).indexOf(k)+1 : -1;
+function solution(num,k){
+    return String(num).includes(k) ? String(num).indexOf(k)+1 : -1;
 }
+
+
+
+// function solution(num, k) {
+//    return String(num).includes(k) ? String(num).indexOf(k)+1 : -1;
+// }
