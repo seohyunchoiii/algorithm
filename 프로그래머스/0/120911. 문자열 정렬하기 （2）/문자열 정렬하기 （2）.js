c704da9b17d792217d597c96ab237c30d60077e5
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-04 20:29:09 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,16 @@
 function solution(my_string) {
-    return my_string.toLowerCase("").split("").sort().join("");
+    return my_string.toLowerCase().split("").sort().join("");
+    
 }
+
+
+
+
+
+
+
+
+
+/*function solution(my_string) {
+    return my_string.toLowerCase("").split("").sort().join("");
+}*/
