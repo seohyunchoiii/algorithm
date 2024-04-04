613bbe7d818fd3a8be4ca0f30208c61f101412a7
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-04 20:37:00 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,7 +1,24 @@
-function solution(n) {
+function solution(n){
+    var pizza = 1;
+    while(6*pizza%n !== 0 ){
+        pizza ++;
+    
+    }
+    return pizza;
+}
+
+
+
+
+
+
+
+
+
+/*function solution(n) {
     let slice = 1;
     while( 6 * slice % n !==0) {
         slice ++;
     }
     return slice;
-}
+}*/
