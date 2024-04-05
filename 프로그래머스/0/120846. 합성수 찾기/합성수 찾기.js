21fce6e469f3e2f00797ea0db30f0a8a6393e9db
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-05 21:58:15 +0900

    [level 0] Title: 합성수 찾기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,4 +1,32 @@
-function solution(n) {
+function solution(n){
+    var answer=0;
+    for(i=1; i<=n; i++){
+        var arr=0;
+        for(j=1; j<=i; j++){
+            if(i%j == 0) {
+                 arr += 1;
+            }
+        }
+       if(arr>=3){
+           answer += 1;
+       }
+    }
+    return answer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function solution(n) {
     var answer = 0;
     for(var i=1; i<=n; i++){
         let counter = 0;
@@ -13,4 +41,4 @@ function solution(n) {
             }
            
     return answer;
-}
+}*/
