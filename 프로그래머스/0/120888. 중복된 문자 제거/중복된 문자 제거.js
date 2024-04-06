4f15f6090f047cd21e9d563edfb79157db52af6f
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-06 14:53:30 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,9 +1,9 @@
 function solution(my_string){
-    return [...new Set(my_string)].join('');
-}
-
-
-
+    return [...my_string].filter((v,index) => [...my_string].indexOf(v) === index).join("");
+}    
+    
+    
+    
 
 /*function solution(my_string) {
     return [...my_string].filter((v,index) => [...my_string].indexOf(v) === index).join('');
