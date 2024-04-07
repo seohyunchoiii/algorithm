897d05d899cd775c372de960ca06f6ad7381c3be
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-07 15:04:08 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,7 +1,10 @@
-function solution(order){
-    return  [...order.toString().matchAll(/3|6|9/g)].length;
-    
+// function solution(order){
+//     return [...order.toString().matchAll(/3|6|9/g)].length;
 
+// }
+
+function solution(order){
+    return String(order).split("").filter( v=> v==3||v==6||v==9).length;
 }
 
 
