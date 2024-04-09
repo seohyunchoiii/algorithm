a2ef645e13cf568e6a85910dbefbb09a8d27c58d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-09 20:37:30 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -1,14 +1,12 @@
-function solution(n){
-    var j=1;
+function solution(n) {
+    var answer=1;
     var i=0;
-    while (j<=n){
+    while(answer<=n){
         i++;
-        j *= i;
+        answer *= i;
     }
     return i-1;
 }
-
-
 
 
 
