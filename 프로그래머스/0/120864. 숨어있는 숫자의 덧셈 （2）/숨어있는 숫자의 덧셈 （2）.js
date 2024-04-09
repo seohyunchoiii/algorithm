6a633d345c770c1ad424f3fa10354e24e7aeea15
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-09 20:43:35 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,12 @@
-function solution(my_string) {
-    return my_string.split(/\D+/).filter(Boolean).reduce((acc,cur) => (+acc) +(+cur),0);
+function solution(my_string){
+    return my_string.split(/\D+/g).filter(Boolean).reduce((acc,cur) => (+acc) + (+cur),0);
 }
+
+
+
+
+
+
+// function solution(my_string) {
+//     return my_string.split(/\D+/).filter(Boolean).reduce((acc,cur) => (+acc) +(+cur),0);
+// }
