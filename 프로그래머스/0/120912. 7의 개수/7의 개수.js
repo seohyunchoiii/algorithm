4da50ae9cfda3506540da364f4b7c5c1ebafa237
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-10 17:07:52 +0900

    [level 0] Title: 7의 개수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -0,0 +1,10 @@
+function solution(array) {
+    var answer = 0;
+    var arr = array.join('').split('');
+    for(i=0; i<arr.length; i++){
+        if(arr[i].includes('7')){
+            answer += 1;
+        }
+    }
+    return answer;
+    }
