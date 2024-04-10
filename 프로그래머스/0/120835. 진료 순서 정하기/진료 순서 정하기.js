cd73274a96584288c327a47eeef6f292686f0452
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-10 16:45:11 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,12 @@
-function solution(emergency) {
-    var sortt = [...emergency].sort((a,b) => (b-a));
-    return emergency.map( el => sortt.indexOf(el)+1);
+function solution(emergency){
+    var sortt = [...emergency].sort((a,b) => b-a);
+    return emergency.map( v => sortt.indexOf(v)+1);
 }
+
+
+
+
+// function solution(emergency) {
+//     var sortt = [...emergency].sort((a,b) => (b-a));
+//     return emergency.map( el => sortt.indexOf(el)+1);
+// }
