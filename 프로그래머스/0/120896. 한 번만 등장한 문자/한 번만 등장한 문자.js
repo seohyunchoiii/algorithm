08ef70c53fbfc55d1282026833d5d897b32064f1
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-10 16:52:37 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,11 +1,29 @@
-function solution(s) {
-    var answer= [];
-    var arr = s.split("");
-    
-    arr.forEach( v => {
-        if(arr.indexOf(v) === arr.lastIndexOf(v)) {
-            answer.push(v)
+function solution(s){
+    var answer = [];
+    for(let a of s) {
+        if( s.indexOf(a) === s.lastIndexOf(a)) {
+            answer.push(a);
         }
-    })
-    return answer.sort().join("");
+    }
+    return answer.sort().join('');
 }
+
+
+
+
+
+
+
+
+
+// function solution(s) {
+//     var answer= [];
+//     var arr = s.split("");
+    
+//     arr.forEach( v => {
+//         if(arr.indexOf(v) === arr.lastIndexOf(v)) {
+//             answer.push(v)
+//         }
+//     })
+//     return answer.sort().join("");
+// }
