76ef0fdd65ad3467908434d974275bb504e302ec
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-13 20:43:08 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,17 +1,19 @@
-function solution(s) {
+function solution(s){
     var answer = 0;
     var arr = s.split(' ');
-    for(var i=0 ; i< arr.length; i++){
+    for( var i =0 ; i<arr.length; i++){
         if(arr[i] !== 'Z') {
             answer += Number(arr[i]);
         }
         else {
             answer -= Number(arr[i-1]);
         }
-        
     }
     return answer;
 }
+
+
+
 
 
 
