c12725202af095d393767ebcf15e8204c17d5a4f
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-14 15:22:15 +0900

    [level 0] Title: 소인수분해, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,10 +1,28 @@
-function solution(n) {
+function solution(n){
     var answer = [];
-    for(i=2; i<=n; i++){
+    for(var i=2; i<=n; i++){
         while(n%i === 0) {
-              answer.push(i);
-              n /= i; 
-            }
-            }
-           return [...new Set(answer)];
+            answer.push(i);
+            n /= i;
         }
+    }
+    return [...new Set(answer)];
+}
+
+
+
+
+
+
+
+
+// function solution(n) {
+//     var answer = [];
+//     for(i=2; i<=n; i++){
+//         while(n%i === 0) {
+//               answer.push(i);
+//               n /= i; 
+//             }
+//             }
+//            return [...new Set(answer)];
+//         }
