81544478d507839ec4252850f91cea4360d3cb8d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-15 20:12:45 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,7 +1,18 @@
-function solution(my_str, n) {
+function solution(my_str,n){
     var answer = [];
-    for(var i=0; i<my_str.length; i +=n ) {
+    for(var i=0; i<my_str.length; i += n){
         answer.push(my_str.slice(i,i+n));
     }
     return answer;
 }
+
+
+
+
+// function solution(my_str, n) {
+//     var answer = [];
+//     for(var i=0; i<my_str.length; i +=n ) {
+//         answer.push(my_str.slice(i,i+n));
+//     }
+//     return answer;
+// }
