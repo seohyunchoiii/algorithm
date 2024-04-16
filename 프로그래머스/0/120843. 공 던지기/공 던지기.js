109fe25869a4453f204faf5e9dfb9821ec686224
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-16 21:57:52 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,4 +1,16 @@
-function solution(numbers, k) {
-    
+function solution(numbers,k){
     return numbers[2*(k-1) % numbers.length];
 }
+
+
+
+
+
+
+
+
+
+// function solution(numbers, k) {
+    
+//     return numbers[2*(k-1) % numbers.length];
+// }
