362779fdf59cd8879701ec8b3ad4d41389e9c56d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-24 20:40:27 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,27 @@
-function solution(n) {
-    let answer = 0;
+function solution(n){
+    let answer=0;
     for(let i=1;i<=n;i++){
-        if(n%i === 0) {
+        if(n%i === 0){
             answer += i;
         }
     }
     return answer;
 }
+
+
+
+
+
+
+
+
+
+// function solution(n) {
+//     let answer = 0;
+//     for(let i=1;i<=n;i++){
+//         if(n%i === 0) {
+//             answer += i;
+//         }
+//     }
+//     return answer;
+// }
