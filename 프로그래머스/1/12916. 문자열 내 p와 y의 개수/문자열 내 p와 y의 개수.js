dd749c9e5236a461c813ca9419c5a1f0af093dcd
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-24 20:15:23 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,3 +1,18 @@
 function solution(s){
-    return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+    return s.toLowerCase().split("p").length === s.toLowerCase().split("y").length;
 }
+
+
+
+
+
+
+
+
+
+// function solution(s){
+//     return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+// console.log( numPY("pPoooyY") )
+// console.log( numPY("Pyy") )
+// }
+// /
