18313433d0ed65cd6437c94047f02a3cd72dad37
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-25 21:58:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,4 +1,11 @@
-function solution(n) {
-    // 삼항 연산자를 이용한 풀이
-    return Math.sqrt(n) % 1 == 0 ? Math.pow(Math.sqrt(n)+1,2) : -1
+function solution(n){
+    return Math.sqrt(n)%1===0 ? Math.pow(Math.sqrt(n)+1,2) : -1;
 }
+
+
+
+
+// function solution(n) {
+//     // 삼항 연산자를 이용한 풀이
+//     return Math.sqrt(n) % 1 == 0 ? Math.pow(Math.sqrt(n)+1,2) : -1
+// }
