f6c74ef05014833a35a49da936920b4b033a16f5
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-26 15:48:40 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,19 @@
 function solution(arr) {
-    var answer=0;
-    for(let i=0; i<arr.length; i++){
-        answer += arr[i];
-    }
-        return answer/arr.length;
+    return arr.reduce((a,b) => a+b)/ arr.length;
 }
+
+
+
+
+
+
+
+
+
+// function solution(arr) {
+//     var answer=0;
+//     for(let i=0; i<arr.length; i++){
+//         answer += arr[i];
+//     }
+//         return answer/arr.length;
+// }
