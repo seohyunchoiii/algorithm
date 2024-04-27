829c7803f2dbc0971aae467d896eeaacc2e2dd16
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-27 22:34:46 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,9 +1,20 @@
-function solution(x) {
+function solution(x){
     let answer = 0;
-    let ha = String(x).split('') ;
-    for(let i=0; i<ha.length; i++){
-         answer += Number(ha[i]);
+    let ha = String(x).split('');
+    for(i=0; i<ha.length; i++){
+        answer += Number(ha[i]);
     }
-    
-         return x% answer == 0 ? true : false;
+    return x%answer == 0 ? true : false;
 }
+
+
+
+// function solution(x) {
+//     let answer = 0;
+//     let ha = String(x).split('') ;
+//     for(let i=0; i<ha.length; i++){
+//          answer += Number(ha[i]);
+//     }
+    
+//          return x% answer == 0 ? true : false;
+// }
