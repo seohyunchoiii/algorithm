6323fabcfbcfec4031e9f2d3ec10385f8c8aa3ee
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-04-29 22:17:02 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,4 +1,15 @@
-function solution(seoul) {
-    let kimm = seoul.indexOf("Kim");
-    return "김서방은 " + kimm + "에 있다";
+function solution(seoul){
+    return "김서방은 " + seoul.indexOf("Kim") + "에 있다"
 }
+
+
+
+
+
+
+
+
+// function solution(seoul) {
+//     let kimm = seoul.indexOf("Kim");
+//     return "김서방은 " + kimm + "에 있다";
+// }
