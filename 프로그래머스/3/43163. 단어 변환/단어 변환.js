387cd82d7dfd83e513b03609d50168790d637a49
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-03 18:19:59 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,39 +1,4 @@
 function solution(begin, target, words) {
-    let answer = 0;
-    let visited =[];
-    let queue = [];
-    
-    if(!words.includes(target)){
-        return 0;
-    }
-    
-    queue.push([begin,answer]);
-    
-    while(queue) {
-        let [v, cnt] = queue.shift();
-        
-        if(v===target){
-            return cnt;
-        }
-        
-        words.forEach(word => {
-            let notEqual = 0;
-            
-            if(visited.includes(word)){
-                return;
-            }
-                
-            for(let i=0; i<word.length; i++){
-                if(word[i] !== v[i]){
-                    notEqual ++;
-                }
-                }
-                if(notEqual === 1){
-                    queue.push([word, ++cnt]);
-                    visited.push(word);
-                }
-            
-        });
-    }
+    var answer = 0;
     return answer;
 }
