5111413914faf3fccbc6526924090ff139955f19
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-04 09:43:01 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,4 +1,89 @@
-function solution(begin, target, words) {
-    var answer = 0;
+function solution(begin,target,words){
+    let answer=0;
+    let visited=[];
+    let queue=[];
+    
+    if(!words.includes(target)){
+        return 0;
+    }
+    
+    queue.push([begin,answer]);
+    
+    while(queue){
+        let [v, cnt] = queue.shift();
+        if(v===target){
+            return cnt;
+        }
+        words.forEach(word => {
+        let notEqual = 0;
+        if(visited.includes(word)){
+            return;
+        }
+        for(let i=0; i<word.length; i++){
+            if(word[i] !== v[i]){
+                notEqual++;
+            }
+        }
+            if(notEqual ===1){
+                queue.push([word,++cnt]);
+                visited.push(word);
+            }
+        
+    });
+    
+    }
+    
+    
+    
     return answer;
 }
+
+
+
+
+
+
+
+
+// function solution(begin,target,words){
+//     let answer=0;
+//     let queue = [];
+//     let visit = [];
+    
+//     if(!words.includes(target)){
+//         return 0;
+//     }
+    
+//     queue.push([begin,answer]);
+    
+//     while(queue){
+//         let [v, cnt] = queue.shift();
+//         if(v===target){
+//             return cnt;
+//         }
+        
+//         words.forEach( word => {
+            
+//             let notEqual =0;
+//             if(visit.includes(word)){
+//                 return;
+//             }
+            
+//             for(let i=0; i<word.length; i++){
+//                 if(word[i] !== v[i]){
+//                     notEqual++;
+//                 }
+//             }
+            
+//             if(notEqual===1){
+//                 queue.push([word, ++cnt]);
+//                 visit.push(word);
+//             }
+                       
+//         })
+        
+                
+//     }
+    
+//     return answer;
+// }
