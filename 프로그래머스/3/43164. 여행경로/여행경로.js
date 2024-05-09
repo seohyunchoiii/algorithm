59b745cd7d40b5a26c955f09e4e7abc188cd410d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-09 21:48:22 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,24 +1,46 @@
+// function solution(tickets){
+//     let routes = [];
+//     dfs(tickets, "ICN", ["ICN"]);
+    
+//     function dfs(allTickets, currentLocation, route) {
+//         if(allTickets.length===0){
+//             routes.push(route);
+//         }
+        
+//         else{
+//             allTickets.forEach(([departure, destination],index) => {
+//                 if(currentLocation ===departure){
+//                     const leftTickets = allTickets.slice();
+//                     leftTickets.splice(index,1);
+                    
+//                     dfs(leftTickets, destination, route.concat(destination));
+//                 }
+//             });
+//         }
+//     }
+    
+//     return routes.sort()[0];
+// }
+
 function solution(tickets){
     let routes = [];
-    dfs(tickets, "ICN", ["ICN"]);
+    dfs(tickets,"ICN",["ICN"]);
     
     function dfs(allTickets, currentLocation, route){
-        if(allTickets.length===0){
+        if(allTickets.length === 0){
             routes.push(route);
         }
-        else {
-            allTickets.forEach(([departure,destination],index) =>
-                               {
-                if(currentLocation===departure){
-                     const leftTickets = allTickets.slice();
+        
+        else{
+            allTickets.forEach(([departure,destination], index) => {
+                if(currentLocation === departure){
+                    const leftTickets = allTickets.slice();
                     leftTickets.splice(index,1);
-                
-                dfs(leftTickets, destination, route.concat(destination));
+                    
+                    dfs(leftTickets,destination,route.concat(destination));
                 }
-                
             });
         }
     }
-    
     return routes.sort()[0];
 }
