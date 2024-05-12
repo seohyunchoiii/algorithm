2a2d2ed38e71ef77431cefc5530e294c2637bf86
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-12 17:17:04 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,13 @@
+function solution(citations) {
+    let answer =0;
+    citations.sort((a,b) => (b-a));
+    
+    for(let i=0; i<citations.length;i++){
+        if(i < citations[i]){
+            answer += 1;
+        }
+        
+    }
+    
+    return answer;
+}
