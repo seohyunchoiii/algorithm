07c32bbbb2652d53c1130cc571cf299426a62630
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-13 16:29:49 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,18 @@
-function solution(numbers) {
-    var answer = numbers.map(v=> v.toString()).sort((a,b) => (b+a) - (a+b)).join('');
+function solution(numbers){
+    let answer = numbers.map(v=>v.toString()).sort((a,b)=> (b+a) - (a+b)).join('');
     
-    return answer[0] === '0'? '0' : answer;
+    
+    return answer[0]==='0' ? '0' : answer;
 }
+
+
+
+
+
+
+
+// function solution(numbers) {
+//     var answer = numbers.map(v=> v.toString()).sort((a,b) => (b+a) - (a+b)).join('');
+    
+//     return answer[0] === '0'? '0' : answer;
+// }
