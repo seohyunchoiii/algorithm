b05c0ed26f25b2840f0750c0b115ee6f0204114c
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-14 20:30:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 56.82 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,14 +1,24 @@
-function solution(arr)
-{
-    var answer = [];
+function solution(arr){
+    return arr.filter((v,i) => v != arr[i+1]);
+}
+
+
+
+
+
+
+
+// function solution(arr)
+// {
+//     var answer = [];
     
-    for(i=0; i<arr.length;i++){
-        if(arr[i]!==arr[i+1]){
-            answer.push(arr[i]);
-        }
-    }
+//     for(i=0; i<arr.length;i++){
+//         if(arr[i]!==arr[i+1]){
+//             answer.push(arr[i]);
+//         }
+//     }
 
     
     
-    return answer;
-}
+//     return answer;
+// }
