0f52e1991bd84876cc84919fc97efca0d6872d09
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-16 20:41:19 +0900

    [level 2] Title: 올바른 괄호, Time: 26.49 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,27 @@
-function solution(s){
-    let cnt=0;
+// function solution(s){
+//     let cnt=0;
     
+    
+//     for(let i=0; i<s.length;i++){
+//         s[i] === '(' ? cnt+=1: cnt -=1;
+//         if(cnt<0){
+//             break;
+//         }
+        
+// }
+//     return cnt === 0 ? true:false;
+// }
+
+function solution(s){
+    let cnt = 0;
     
     for(let i=0; i<s.length;i++){
-        s[i] === '(' ? cnt+=1: cnt -=1;
+        s[i] === '(' ? cnt+=1: cnt-=1;
         if(cnt<0){
             break;
         }
-        
+    }
+    return cnt===0? true:false;
 }
-    return cnt === 0 ? true:false;
-}
+
+
