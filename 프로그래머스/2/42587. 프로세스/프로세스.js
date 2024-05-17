6b48f20d5940901906f587c71e15b538cfa846f8
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-17 09:54:37 +0900

    [level 2] Title: 프로세스, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,26 +1,61 @@
-function solution(priorities, location) {
-    var answer = 0;
-    let q = [];
-    
+function solution(priorities,location){
+    let answer = 0;
+    let q =[];
     
     for(let i=0; i<priorities.length;i++){
         q.push(i);
     }
     
-    while(q.length>0){
+    while(priorities.length>0){
         let max = Math.max(...priorities);
+        
         if(priorities[0]<max){
             priorities.push(priorities.shift());
             q.push(q.shift());
         }
         else {
+            answer +=1;
             priorities.shift();
-            answer += 1;
-            if(q.shift() ===location){
-                return answer;
-            }
+             if(q.shift()===location){
+            return answer;
         }
+        }
+       
     }
     
-    
 }
+
+
+
+
+
+
+
+
+
+// function solution(priorities, location) {
+//     var answer = 0;
+//     let q = [];
+    
+    
+//     for(let i=0; i<priorities.length;i++){
+//         q.push(i);
+//     }
+    
+//     while(q.length>0){
+//         let max = Math.max(...priorities);
+//         if(priorities[0]<max){
+//             priorities.push(priorities.shift());
+//             q.push(q.shift());
+//         }
+//         else {
+//             priorities.shift();
+//             answer += 1;
+//             if(q.shift() ===location){
+//                 return answer;
+//             }
+//         }
+//     }
+    
+    
+// }
