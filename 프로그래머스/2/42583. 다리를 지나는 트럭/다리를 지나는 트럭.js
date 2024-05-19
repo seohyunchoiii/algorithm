0793b158f3b9e21283c829074c47b55402868cdc
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-19 11:34:05 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,18 +1,43 @@
-function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    let bridge= Array.from({length:bridge_length}, ()=>0);
-    let bridgeWeight = 0;
+// function solution(bridge_length, weight, truck_weights) {
+//     var answer = 0;
+//     let bridge= Array.from({length:bridge_length}, ()=>0);
+//     let bridgeWeight = 0;
+    
+//     answer++;
+//     bridge.shift();
+//     bridgeWeight+= truck_weights[0];
+//     bridge.push(truck_weights.shift());
+    
+//     while(bridgeWeight>0){
+//         answer++;
+//         bridgeWeight -= bridge.shift();
+//         if(truck_weights.length>0&&bridgeWeight+truck_weights[0]<=weight){
+//             bridgeWeight+= truck_weights[0];
+//             bridge.push(truck_weights.shift());
+//         }
+//         else{
+//             bridge.push(0);
+//         }
+//     }
+    
+//     return answer;
+// }
+
+function solution(bridge_length,weight,truck_weights){
+    let answer=0;
+    let bridge_weight =0;
+    let bridge = Array.from({length:bridge_length}, ()=>0);
     
     answer++;
     bridge.shift();
-    bridgeWeight+= truck_weights[0];
+    bridge_weight += truck_weights[0];
     bridge.push(truck_weights.shift());
     
-    while(bridgeWeight>0){
+    while(bridge_weight>0){
         answer++;
-        bridgeWeight -= bridge.shift();
-        if(truck_weights.length>0&&bridgeWeight+truck_weights[0]<=weight){
-            bridgeWeight+= truck_weights[0];
+        bridge_weight-=bridge.shift();
+        if(truck_weights.length>0&&bridge_weight+truck_weights[0]<=weight){
+            bridge_weight += truck_weights[0];
             bridge.push(truck_weights.shift());
         }
         else{
@@ -22,3 +47,4 @@ function solution(bridge_length, weight, truck_weights) {
     
     return answer;
 }
+   
