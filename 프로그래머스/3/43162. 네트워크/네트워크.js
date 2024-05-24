9e03a40f1bfd88d2bf4ec26a2f4d41e8938a817b
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-24 20:52:27 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -21,22 +21,24 @@
 
 function solution(n,computers){
     let answer=0;
-    let visited = Array.from({length:n},() => false);
+    const visited = Array.from({length:n}, () => false);
     
     function dfs(i){
-        visited[i]=true;
-        for(let j=0;j<n;j++){
-            if(!visited[j]&&computers[i][j]===1){
+        visited[i] = true;
+        for(let j=0;j<computers.length;j++){
+            if(!visited[j]&& computers[i][j]===1){
                 dfs(j);
             }
         }
-    }
-    
-    for(let j=0;j<n;j++){
-        if(!visited[j]){
-            dfs(j);
-            answer++;
+       
+            }
+     for(let j=0;j<n;j++){
+            if(!visited[j]){
+                dfs(j);
+                answer++;
         }
     }
+    
+    
     return answer;
 }
