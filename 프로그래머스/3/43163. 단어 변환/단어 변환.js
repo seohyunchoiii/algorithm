3321897a0b5944b485650b1f34143053e0bf4cec
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-24 22:08:25 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,50 +1,3 @@
-function solution(begin,target,words){
-    let answer=0;
-    let visited=[];
-    let queue=[];
-    
-    if(!words.includes(target)){
-        return 0;
-    }
-    
-    queue.push([begin,answer]);
-    
-    while(queue){
-        let [v, cnt] = queue.shift();
-        if(v===target){
-            return cnt;
-        }
-        words.forEach(word => {
-        let notEqual = 0;
-        if(visited.includes(word)){
-            return;
-        }
-        for(let i=0; i<word.length; i++){
-            if(word[i] !== v[i]){
-                notEqual++;
-            }
-        }
-            if(notEqual ===1){
-                queue.push([word,++cnt]);
-                visited.push(word);
-            }
-        
-    });
-    
-    }
-    
-    
-    
-    return answer;
-}
-
-
-
-
-
-
-
-
 // function solution(begin,target,words){
 //     let answer=0;
 //     let queue = [];
@@ -87,3 +40,35 @@ function solution(begin,target,words){
     
 //     return answer;
 // }
+
+function solution(begin,target,words){
+    let answer=0;
+    let visit=[];
+    let q=[];
+    
+    if(!words.includes(target)) return 0;
+    q.push([begin,answer]);
+    
+    while(q.length>0){
+        let [v,cnt] = q.shift();
+        if(v===target){
+            return cnt;
+        }
+        
+        words.forEach(word => {
+            let notEqual = 0;
+            if(visit.includes(word)) return;
+            for(let i=0; i<target.length;i++){
+                if(word[i] !== v[i]){
+                    notEqual++;
+                }
+            }
+            if(notEqual===1){
+                    q.push([word,++cnt]);
+                    visit.push(word);
+                }
+        });
+        
+    }
+    return answer;
+}
