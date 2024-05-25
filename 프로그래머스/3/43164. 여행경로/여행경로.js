984870a9f47f82f232809ecef1c82bf67576167d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-25 21:12:44 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -23,24 +23,25 @@
 // }
 
 function solution(tickets){
-    let routes = [];
+    let q=[];
     dfs(tickets,"ICN",["ICN"]);
     
-    function dfs(allTickets, currentLocation, route){
-        if(allTickets.length === 0){
-            routes.push(route);
+    function dfs(alltickets,current,route){
+        if(alltickets.length===0){
+            q.push(route);
         }
-        
         else{
-            allTickets.forEach(([departure,destination], index) => {
-                if(currentLocation === departure){
-                    const leftTickets = allTickets.slice();
-                    leftTickets.splice(index,1);
+            alltickets.forEach(([departure,destination],index) => {
+                if(current===departure){
+                    let newtickets = alltickets.slice();
+                    newtickets.splice(index,1);
                     
-                    dfs(leftTickets,destination,route.concat(destination));
+                    dfs(newtickets,destination,route.concat(destination));
                 }
             });
         }
-    }
-    return routes.sort()[0];
+    
+        
+}
+    return q.sort()[0];
 }
