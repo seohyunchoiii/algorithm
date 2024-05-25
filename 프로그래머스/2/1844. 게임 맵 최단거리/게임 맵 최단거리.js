f77f2606494796834a024d82c87f4bcac4e4e46c
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-05-25 17:18:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.65 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,8 +1,37 @@
+// function solution(maps){
+//     var dir = [[-1,0],[1,0],[0,-1],[0,1]];
+    
+//     var bfs = () => {
+//         var q= [[0,0,1]];
+//         maps[0][0] = 0;
+        
+//         while(q.length>0){
+//             var[row,col,distance] = q.shift();
+            
+//             if(row==maps.length-1&&col==maps[0].length-1){
+//                 return distance;
+//             }
+//             for(var [r,c] of dir){
+//                 var newRow = row + r;
+//                 var newCol = col + c;
+                
+//                 if(newRow>=0&&newRow<maps.length&&newCol>=0&&newCol<maps[0].length&&maps[newRow][newCol]===1){
+//                     q.push([newRow,newCol,distance+1]);
+//                     maps[newRow][newCol] = 0;
+//                 }
+//             }
+//         }
+//         return -1;
+//     };
+//     return bfs();
+// }
+
 function solution(maps){
     var dir = [[-1,0],[1,0],[0,-1],[0,1]];
+
     
     var bfs = () => {
-        var q= [[0,0,1]];
+        var q = [[0,0,1]];
         maps[0][0] = 0;
         
         while(q.length>0){
@@ -11,7 +40,8 @@ function solution(maps){
             if(row==maps.length-1&&col==maps[0].length-1){
                 return distance;
             }
-            for(var [r,c] of dir){
+            
+            for(var[r,c] of dir){
                 var newRow = row + r;
                 var newCol = col + c;
                 
@@ -25,31 +55,3 @@ function solution(maps){
     };
     return bfs();
 }
-
-// function solution(maps){
-//     var dir = [[-1,0],[1,0],[0,-1],[0,1]];
-    
-//     var bfs = () => {
-//         var q = [[0,0,1]];
-//         maps[0][0]=0;
-        
-//         while(q.length>0){
-//             var[row,col,distance] = q.shift();
-//             if(row==maps.length-1&&col==maps[0].length-1){
-//                 return distance;
-//             }
-            
-//             for(var [r,c] of dir){
-//                 var newRow = row+r;
-//                 var newCol = col+c;
-                
-//                 if(newRow>=0&&newRow<maps.length&&newCol>=0&&newCol<maps[0].length&&maps[newRow][newCol]===1){
-//                     q.push([newRow,newCol,distance+1]);
-//                     maps[newRow][newCol]=0;
-//                 }
-//             }
-//         }
-//         return -1;
-//     };
-//     return bfs();
-// }
