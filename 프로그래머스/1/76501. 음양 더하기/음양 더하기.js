234e49c3b58778e23c3ad22ac62e6ebbfee9e9e9
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-06-09 16:54:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,12 +1,8 @@
-function solution(absolutes, signs) {
-    var answer = 0;
-    for(let i=0;i<absolutes.length;i++){
-        if(signs[i]===true){
-            answer = answer + absolutes[i];
-        }
-        else {
-            answer = answer - absolutes[i];
-        }
+function solution(absolutes,signs){
+    let answer=0;
+    for (let i=0; i<absolutes.length;i++){
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i];
     }
+    
     return answer;
 }
