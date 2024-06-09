683c4be50e337dc5433cb18b0d6934fc7a363e61
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-06-09 17:21:48 +0900

    [level 3] Title: 입국심사, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,15 +1,33 @@
-function solution(n, times){
-    const sorted = times.sort((a,b) => a - b);
-    let start = 0;
-    let end = sorted[sorted.length-1] * n;
+// function solution(n, times){
+//     const sorted = times.sort((a,b) => a - b);
+//     let start = 0;
+//     let end = sorted[sorted.length-1] * n;
+    
+//     while(start<=end){
+//         const mid = Math.floor((start+end)/2);
+//         const sum = times.reduce((acc,cur) => acc+Math.floor(mid/cur),0);
+//         if(sum<n){
+//             start = mid+1;
+//         }
+//         else{
+//             end = mid-1;
+//         }
+//     }
+//     return start;
+// }
+
+function solution(n,times){
+    const sorted = times.sort((a,b) => a-b);
+    let start=0;
+    let end=sorted[sorted.length-1]*n;
     
     while(start<=end){
-        const mid = Math.floor((start+end)/2);
-        const sum = times.reduce((acc,cur) => acc+Math.floor(mid/cur),0);
+        let mid = Math.floor((start+end)/2);
+        let sum = sorted.reduce((acc,cur)=>acc+Math.floor(mid/cur),0);
         if(sum<n){
             start = mid+1;
         }
-        else{
+        else {
             end = mid-1;
         }
     }
