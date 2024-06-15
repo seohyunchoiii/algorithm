193a3e99bf3843f81d93a8594206ed7e254f3ba9
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-06-15 22:50:24 +0900

    [level 4] Title: 징검다리, Time: 25.34 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.js
+++ 프로그래머스/4/43236. 징검다리/징검다리.js
@@ -1,29 +1,31 @@
-const solution = (distance, rocks, n) => {
-  rocks.push(0);
-  rocks.push(distance);
-  rocks.sort((a, b) => a - b);
-  const dist = [];
-  for (let i = 1; i < rocks.length; i++) {
-    dist.push(rocks[i] - rocks[i - 1]);
-  }
-  let max = distance;
-  let min = 0;
-  let ans = 0;
-  while (min <= max) {
-    const mid = Math.floor((max + min) / 2);
-    let acc = 0;
-    let cnt = 0;
-    for (const value of dist) {
-      if (value + acc < mid) {
-        acc += value;
-        cnt++;
-      } else acc = 0;
+function solution(distance, rocks, n) {
+    let answer = 0;
+    rocks=[0,...rocks.sort((a,b)=>a-b),distance];
+    
+    const BinarySearch=()=>{
+        let start=0;
+        let end=rocks[rocks.length-1];
+        
+        while(start<=end){
+            let mid=Math.floor((start+end)/2);
+            let count=0,now=0;
+            for(let i=1;i<rocks.length;i++){
+                if(rocks[i]-now<mid){
+                    count++;   
+                }else{
+                    now=rocks[i];
+                }
+            }
+            
+            if(count>n){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        answer=end;
     }
-    if (cnt > n) max = mid - 1;
-    else {
-      min = mid + 1;
-      ans = mid;
-    }
-  }
-  return ans;
-};
+    
+    BinarySearch();
+    return answer;
+}
