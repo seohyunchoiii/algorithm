39353ff9bae5010c71bd6d7fca6776f0071f2362
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-06-21 20:46:19 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,10 +1,21 @@
-function solution(numbers) {
-    var answer = 0;
+// function solution(numbers) {
+//     var answer = 0;
+    
+//     for(let i=0; i<10; i++){
+//       if(!numbers.includes(i)){
+//           answer += i;
+//       }
+//     }
+//     return answer;
+// }
+
+function solution(numbers){
+    let answer=0;
     
     for(let i=0; i<10; i++){
-      if(!numbers.includes(i)){
-          answer += i;
-      }
+        if(!numbers.includes(i)){
+            answer += i;
+        }
     }
     return answer;
 }
