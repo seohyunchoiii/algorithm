7336476cd94ad9de6a56113dd6cdac1d0d08e3c6
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-06-22 22:05:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,10 +1,20 @@
-function solution(num) {
-    var answer = 0;
+// function solution(num) {
+//     var answer = 0;
     
-    while(num!=1 && answer !== 500){
-        num%2==0 ? num=num/2 : num=num*3 +1;
-        answer++;
+//     while(num!=1 && answer !== 500){
+//         num%2==0 ? num=num/2 : num=num*3 +1;
+//         answer++;
+//     }
+    
+//     return num===1 ? answer : -1;
+// }
+
+function solution(n){
+    let answer = 0;
+    
+    while(n!=1 && answer !==500){
+        n%2 == 0 ? n = n/2 : n = n*3+1
+        answer ++;
     }
-    
-    return num===1 ? answer : -1;
+    return n === 1 ? answer : -1;
 }
