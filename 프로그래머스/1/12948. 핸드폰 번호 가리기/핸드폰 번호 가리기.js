2f8907a8730c036ec096aa32032e7e7a47a7aca3
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-06-26 22:11:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,12 @@
-function solution(phone_number) {
-    var answer = '';
+// function solution(phone_number) {
+//     var answer = '';
+//     answer = phone_number.slice(0,-4).replace(/[0-9]/g,'*') + phone_number.slice(-4)
+//     return answer;
+// }
+
+function solution(phone_number){
+    let answer = '';
     answer = phone_number.slice(0,-4).replace(/[0-9]/g,'*') + phone_number.slice(-4)
+    
     return answer;
 }
