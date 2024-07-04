2c0860f8154c4d2a305a99e147f7dc07d7ef1801
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-07-04 23:21:25 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,11 +1,14 @@
-function solution(arr) {
-    var answer = [];
-        if(arr.length===1){
-           return [-1]
-        }
-        else {
-           arr.splice(arr.indexOf(Math.min(...arr)),1);
-           answer = arr;
+
+// function solution(arr) {
+//     arr.splice(arr.indexOf(Math.min(...arr)),1);
+//     if(arr.length<1)return[-1];
+//     return arr;
+// }
+
+function solution(arr){
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1){
+        return [-1];
     }
-    return answer;
+    return arr;
 }
