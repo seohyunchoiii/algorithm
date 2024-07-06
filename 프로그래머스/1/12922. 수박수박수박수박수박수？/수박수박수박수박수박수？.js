b4e44a4c5217bb452e3e8e8723ecf172f60605bd
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-07-06 21:51:20 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,12 +1,18 @@
-function solution(n) {
-    var answer = '';
-    for(let i=0; i<n; i++){
-        if(i%2===0){
-            answer += '수'
-        }
-        else {
-            answer += '박'
-        }
-    }
-    return answer;
+// function solution(n) {
+//     var answer = '';
+//     for(let i=0; i<n; i++){
+//         if(i%2===0){
+//             answer += '수'
+//         }
+//         else {
+//             answer += '박'
+//         }
+//     }
+//     return answer;
+// }
+
+// 
+
+function solution(n){
+    return "수박".repeat(n).substring(0,n);
 }
