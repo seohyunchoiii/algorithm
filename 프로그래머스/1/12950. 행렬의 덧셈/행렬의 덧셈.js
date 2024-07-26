0f0ab6fe1dc39fbd8be7d57c43f5667043abc148
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-07-26 22:13:19 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.18 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,23 @@
-function solution(arr1, arr2) {
-    var answer = [];
-    for(let i=0; i<arr1.length; i++){
-        let sum=[];
-        for( let j=0; j<arr1[i].length; j++){
-           sum.push(arr1[i][j] + arr2[i][j]);
+// function solution(arr1, arr2) {
+//     var answer = [];
+//     for(let i=0; i<arr1.length; i++){
+//         let sum=[];
+//         for( let j=0; j<arr1[i].length; j++){
+//            sum.push(arr1[i][j] + arr2[i][j]);
+//         }
+//         answer.push(sum);
+//     }
+//     return answer;
+// }
+
+function solution(arr1,arr2){
+    let answer = [];
+    for(let i=0; i<arr1.length;i++){
+        let sum =[];
+        for(let j=0; j<arr1[i].length;j++){
+            sum.push(arr1[i][j]+arr2[i][j]);
         }
-        answer.push(sum);
+        answer.push(sum)
     }
     return answer;
 }
