660351d0a9921c886ab0dd23b738e9cf3522ebd8
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-03 06:29:48 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,26 +1,13 @@
-// function solution(n, m){
-//     var answer = [];
-    
-//     const gcf = (a,b) => {
-//         if (b===0) {
-//             return a;
-//         }
-//         return gcf(b, a%b)
-//     }
-//     const lcm = (a,b) => (a*b) / gcf(a,b)
-    
-//     return [gcf(n,m), lcm(n,m)]
-// }
-
-function solution(n,m){
+function solution(n, m){
     var answer = [];
-    let gcf = (a,b) => {
-        if (b===0){
+    
+    const gcf = (a,b) => {
+        if (b===0) {
             return a;
         }
-        return gcf(b,a%b);
+        return gcf(b, a%b)
     }
-    let lcm =(a,b) => (a*b) / gcf(a,b);
+    const lcm = (a,b) => (a*b) / gcf(a,b)
     
-    return [gcf(n,m), lcm(n,m)];
+    return [gcf(n,m), lcm(n,m)]
 }
