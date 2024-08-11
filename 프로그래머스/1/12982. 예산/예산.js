64ca8e4ffac226127e7a37b4396823cfda1eefa2
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-11 21:31:52 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,8 +1,23 @@
-function solution(d, budget) {
-    var answer = 0, sum=0;
-    d.sort((a,b) => a - b);
+// function solution(d, budget) {
+//     var answer = 0, sum=0;
+//     d.sort((a,b) => a - b);
     
-    for(let i=0;i<d.length;i++){
+//     for(let i=0;i<d.length;i++){
+//         answer++;
+//         sum += d[i];
+        
+//         if(sum>budget){
+//             answer--;
+//         }
+//     }
+//     return answer;
+// }
+
+function solution(d,budget){
+    var answer=0, sum=0;
+    d.sort((a,b) => a-b);
+    
+    for(let i=0; i<d.length; i++){
         answer++;
         sum += d[i];
         
@@ -10,5 +25,6 @@ function solution(d, budget) {
             answer--;
         }
     }
+    
     return answer;
 }
