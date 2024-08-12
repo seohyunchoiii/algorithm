a49273084cc799fb3b9bcd2789549c3eec8d6900
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-12 21:49:20 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,5 +1,11 @@
+// function solution(n) {
+//     let answer = n.toString(3).split('').reverse().join('');
+    
+//     return parseInt(answer,3);
+// }
+
 function solution(n) {
     let answer = n.toString(3).split('').reverse().join('');
     
-    return parseInt(answer,3);
+    return parseInt(answer,3)
 }
