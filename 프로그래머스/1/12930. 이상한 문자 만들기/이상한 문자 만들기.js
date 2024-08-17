f0848dca45ba3e098cb002d06aa2e8dd453a906f
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-17 23:20:28 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,13 +1,32 @@
-function solution(s) {
+// function solution(s) {
+//     var answer = '';
+//     let words = s.split(' ')
+    
+//     for(let i=0; i<words.length;i++){
+//         for(let j=0; j<words[i].length;j++){
+//             if(j % 2 === 0){
+//                 answer += words[i][j].toUpperCase();
+//             }
+//             else{
+//                 answer += words[i][j].toLowerCase();
+//             }
+//         }
+//         if(i<words.length-1){
+//             answer += ' ';
+//         }
+//     }
+//     return answer;
+// }
+function solution(s){
     var answer = '';
-    let words = s.split(' ')
+    let words = s.split(' ');
     
     for(let i=0; i<words.length;i++){
         for(let j=0; j<words[i].length;j++){
-            if(j % 2 === 0){
-                answer += words[i][j].toUpperCase();
-            }
-            else{
+           if(j%2===0){
+               answer += words[i][j].toUpperCase();
+           }
+            else {
                 answer += words[i][j].toLowerCase();
             }
         }
