f3a8f67af81e0053c75ce0b53374b8f86e4f752d
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-20 21:02:29 +0900

    [level 1] Title: 삼총사, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,12 +1,24 @@
-function solution(number) {
-   let res = [];
+// function solution(number) {
+//    let res = [];
 
-    for(let i=0;i<number.length;i++){
+//     for(let i=0;i<number.length;i++){
+//         for(let j=i+1; j<number.length;j++){
+//             for(let k=j+1; k<number.length;k++){
+//                 res.push(number[i]+number[j]+number[k]);
+//             }
+//         }
+//     }
+//     return res.filter((re) => re===0).length;
+// }
+
+function solution(number){
+    let answer = [];
+    for(let i=0; i<number.length;i++){
         for(let j=i+1; j<number.length;j++){
             for(let k=j+1; k<number.length;k++){
-                res.push(number[i]+number[j]+number[k]);
+                answer.push(number[i]+number[j]+number[k]);
             }
         }
     }
-    return res.filter((re) => re===0).length;
+    return answer.filter((el) => el===0).length;
 }
