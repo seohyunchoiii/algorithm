f93add740060617f9db5de70ebd2bd02a4fa57ce
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-21 21:14:46 +0900

    [level 1] Title: 최소직사각형, Time: 5.08 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,13 @@
+function solution(sizes) {
+    var answer = 0;
+    var width = [];
+    var height = [];
+    /*max of max, max of min 찾기 */
+    for(let i=0; i<sizes.length;i++){
+        const max = Math.max(sizes[i][0],sizes[i][1]);
+        const min = Math.min(sizes[i][0],sizes[i][1]);
+        width.push(max);
+        height.push(min);
+    }
+        return Math.max(...width) * Math.max(...height);
+}
