78d82b060ab4079d73056a9d9806e23f02228be3
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-24 22:45:46 +0900

    [level 1] Title: 시저 암호, Time: 3.14 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,22 +1,44 @@
-function solution(s, n) {
-    var answer = '';
-    for(let i=0;i<s.length;i++){
+// function solution(s, n) {
+//     var answer = '';
+//     for(let i=0;i<s.length;i++){
+//         let sASCII = s.charCodeAt(i);
+//         if(sASCII >=65 && sASCII <=90){
+//             sASCII += n;
+//             if(sASCII > 90) {
+//                 sASCII -=26;
+//             }
+//         }
+//         else if(sASCII >=97 && sASCII <=122){
+//             sASCII += n;
+//             if(sASCII > 122) {
+//                 sASCII -=26;
+//             }
+//         }
+//     let sString = String.fromCharCode(sASCII);
+//     answer += sString;
+//     }
+//     return answer;
+// }
+
+function solution(s,n){
+    let answer = '';
+    for(let i=0; i<s.length;i++){
         let sASCII = s.charCodeAt(i);
-        if(sASCII >=65 && sASCII <=90){
+        if(sASCII >=65 && sASCII <= 90){
             sASCII += n;
-            if(sASCII > 90) {
-                sASCII -=26;
+            if(sASCII > 90){
+                sASCII -= 26;
             }
         }
-        else if(sASCII >=97 && sASCII <=122){
+        else if(sASCII>=97 && sASCII <=122){
             sASCII += n;
-            if(sASCII > 122) {
-                sASCII -=26;
+            if(sASCII > 122){
+                sASCII -= 26;
             }
         }
-    let sString = String.fromCharCode(sASCII);
-    answer += sString;
+        sString = String.fromCharCode(sASCII);
+        answer += sString;
     }
+    
     return answer;
 }
-
