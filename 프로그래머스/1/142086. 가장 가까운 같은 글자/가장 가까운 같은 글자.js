38b0e1af2d25fab4b70387d069d4d50e3f52ccab
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-08-27 22:25:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 19.49 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = [];
+    var temp = [];
+    for(let i=0; i<s.length; i++){
+        if(!temp.includes(s[i])){
+            answer.push(-1);
+            temp.push(s[i]);
+        }
+        else if(temp.includes(s[i])){
+            answer.push(i - temp.lastIndexOf(s[i]));
+            temp.push(s[i]);
+        }
+    }
+    return answer;
+}
