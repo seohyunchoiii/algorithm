0bd97737b8705eda852f4584a86aaa2d8b2bd963
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-09-04 21:59:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 6.55 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    var answer = s;
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for(let i=0; i<numbers.length;i++){
+        let arr = answer.split(numbers[i]);
+        console.log(arr)
+        answer = arr.join(i)
+    }
+    
+    return Number(answer);
+}
