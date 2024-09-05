ce2a1938c9db4d4e2094d1047cd76f6bdab41ac3
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-09-05 21:55:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,12 +1,23 @@
-function solution(s) {
+// function solution(s) {
+//     var answer = s;
+//     let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+//     for(let i=0; i<numbers.length;i++){
+//         let arr = answer.split(numbers[i]);
+//         
+//         answer = arr.join(i)
+//     }
+    
+//     return Number(answer);
+// }
+
+function solution(s){
     var answer = s;
-    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let numbers = ["zero", "one", "two", "three","four", "five", "six", "seven","eight", "nine"];
     
-    for(let i=0; i<numbers.length;i++){
+    for(let i=0;i<numbers.length;i++){
         let arr = answer.split(numbers[i]);
-        console.log(arr)
-        answer = arr.join(i)
+        answer = arr.join(i);
     }
-    
     return Number(answer);
 }
