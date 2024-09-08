1df5b693230fd808cc0393dceb7b5c61454b4239
seohyunchoiii <164629967+seohyunchoiii@users.noreply.github.com>
2024-09-08 20:42:45 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,7 +1,15 @@
-function solution(food) {
-    var answer = '';
+// function solution(food) {
+//     var answer = '';
+//     for(let i=1; i<food.length;i++){
+//         answer += String(i).repeat(Math.floor(food[i]/2));
+//     }
+//     return answer + "0" + [...answer].reverse().join("");
+// }
+
+function solution(food){
+    let answer = '';
     for(let i=1; i<food.length;i++){
         answer += String(i).repeat(Math.floor(food[i]/2));
     }
-    return answer + "0" + [...answer].reverse().join("");
+    return answer + "0" + [...answer].reverse().join('');
 }
